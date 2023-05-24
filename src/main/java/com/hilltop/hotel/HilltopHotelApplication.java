413cfa53bd3da3e3a6181eac19738f219236fbb8
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-05-24 17:15:58 +0530

    Initial commit

---
+++ src/main/java/com/hilltop/hotel/HilltopHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hilltop.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilltopHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HilltopHotelApplication.class, args);
+	}
+
+}
